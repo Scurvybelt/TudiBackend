981037740f1a96fd598b42ff432a903d338799f8
Scurvybelt <90409656+Scurvybelt@users.noreply.github.com>
2025-10-22 07:49:40 -0600

    Ruta base de datos

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = 'mysql+pymysql://root:admin@localhost:3306/tudi'
+URL_DATABASE = 'mysql+pymysql://EduardoTudi:Eduardo%40Tudi2025@localhost:3306/tudi'
 
 engine = create_engine(URL_DATABASE)
 
